0c883912f3763821c6ae62b92f5a711e908fa39f
Daryna Bilan <rinabkh98@gmail.com>
2023-08-23 23:52:50 +0300

    finished work #main

--- index.js
+++ index.js
@@ -2,30 +2,55 @@ const currentDate = new Date();
 
 let sec = currentDate.getSeconds();
 let min = currentDate.getMinutes();
-// const dateAdd = () => {
-//   $("#date").append(
-//     currentDate.getHours() +
-//       ":" +
-//       currentDate.getMinutes() +
-//       ":" +
-//       currentDate.getSeconds()
-//   );
-// };
+let hour = currentDate.getHours();
+
+// sec = 55;
+// min = 59;
+// hour = 1;
 
 $(document).ready(function () {
+  let startedHours = $("#hours").append(hour);
+  let startedMin = $("#min").append(min);
+  let startedSec = $("#sec").append(sec);
+
   setInterval(function () {
-    if (sec == 60) {
-      sec = 0;
+    if (sec == 59) {
+      sec = -1;
       sec++;
 
-      if (min == 60) {
-        min = 0;
+      if (min == 59) {
+        min = -1;
         min++;
+
+        if (hour == 23) {
+          hour = -1;
+          hour++;
+          startedHours.html("00");
+        } else {
+          hour++;
+          if (hour < 10) {
+            startedHours.html("0" + hour);
+          } else {
+            startedHours.html(hour);
+          }
+        }
+        startedMin.html("0" + min);
+      } else {
+        min++;
+        if (min < 10) {
+          startedMin.html("0" + min);
+        } else {
+          startedMin.html(min);
+        }
       }
-      min++;
+      startedSec.html("00");
     } else {
       sec++;
+      if (sec < 10) {
+        startedSec.html("0" + sec);
+      } else {
+        startedSec.html(sec);
+      }
     }
-    console.log(min, sec);
   }, 1000);
 });
